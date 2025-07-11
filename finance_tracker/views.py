96b96b430910f198e677ec619a79e931b087ef01
tazawaakira <a.tazawa.sys24@morijyobi.ac.jp>
2025-07-11 14:19:36 +0900

    編集と削除機能

--- finance_tracker/views.py
+++ finance_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Transaction
 from .forms import TransactionForm
 
@@ -6,7 +6,7 @@ def root_redirect(request):
     return redirect('login')
 
 def transaction_list(request):
-    transactions = Transaction.objects.filter(user=request.user).order_by('date')  # 昇順に並べ替え
+    transactions = Transaction.objects.filter(user=request.user).order_by('date')
     balance = 0
     transaction_with_balance = []
 
@@ -19,6 +19,7 @@ def transaction_list(request):
             expense = t.amount
         balance += income - expense
         transaction_with_balance.append({
+            'id': t.id,
             'date': t.date,
             'category': t.category,
             'description': t.description,
@@ -38,10 +39,31 @@ def add_transaction(request):
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.user = request.user  # ユーザーを設定
+            transaction.user = request.user
             transaction.save()
             return redirect('transaction_list')
     else:
         form = TransactionForm()
 
     return render(request, 'finance_tracker/add_transaction.html', {'form': form})
+
+def edit_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, 'finance_tracker/edit_transaction.html', {'form': form})
+
+from django.shortcuts import render, redirect, get_object_or_404
+
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('transaction_list')
+    return render(request, 'finance_tracker/delete_transaction_confirm.html', {'transaction': transaction})
+
